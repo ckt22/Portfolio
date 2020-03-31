a0a80d85adbaf0aede591024f3abdb634000bc8d
CK TANG <cktangad@connect.ust.hk>
2020-03-31 21:54:34 +0800

    Edited description

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,14 +39,15 @@ const About = () => {
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
                   {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'I am a year 3 student in HKUST, majoring in Computer Engineering (CPEG).'}
                 </p>
                 <p className="about-wrapper__info-text">
                   {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'I am highly passionate of technology. Though I have equal interest in all areas of IT, I am more experienced in full-stack software development. '}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  {paragraphThree ||
+                    'This page serves as my portfolio for sharing my recent work. Scroll down to learn more!'}
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
