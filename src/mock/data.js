db43cac9d6e004d69430a69245a56e51022a4222
CK TANG <cktangad@connect.ust.hk>
2020-03-31 18:48:24 +0800

    sdfsddsf

--- src/mock/data.js
+++ src/mock/data.js
@@ -3,63 +3,57 @@ import uuidv1 from 'uuid/v1';
 // HEAD DATA
 export const headData = {
   title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi! I am',
+  name: 'Kit',
+  subtitle: 'And here is my portfolio.',
+  cta: 'Check me out!',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profilePic.jpg',
+  paragraphOne: 'I am a year 3 student in HKUST, majoring in Computer Engineering (CPEG).',
+  paragraphTwo:
+    'I am highly passionate of technology. Though I have equal interest in all areas of IT, I am more experienced in full-stack software development. ',
+  paragraphThree:
+    'This page serves as my portfolio for sharing my recent work. Scroll down to learn more!',
+  resume: 'https://cktangad.student.ust.hk/resume.pdf', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'Q&A_Customer_Portal_Selected.png',
+    title: 'Product Q&A Customer Portal for mysilkway.com',
+    info:
+      'This Q&A box appears on all SKU pages. Logged in customers may ask a question about the product.',
+    info2: 'Customers can also view published questions and rate them.',
+    url: 'https://www.mysilkway.com/',
+    // repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'Q&A_Admin_Portal_Selected.png',
+    title: 'Product Q&A Admin Portal for mysilkway.com',
+    info: 'The admin portal handles received Q&As for all SKUs.',
+    info2: 'It supports functions such as publish, unpublish, reply and ignore questions.',
+    // url: 'https://www.mysilkway.com/',
+    // repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'I am more than thrilled for a chat!',
+  btn: 'Email me!',
+  email: 'cktangad@connect.ust.hk',
 };
 
 // FOOTER DATA
@@ -67,28 +61,13 @@ export const footerData = {
   networks: [
     {
       id: uuidv1(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: uuidv1(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: uuidv1(),
       name: 'linkedin',
-      url: '',
-    },
-    {
-      id: uuidv1(),
-      name: 'github',
-      url: '',
+      url: 'https://www.linkedin.com/in/cheuk-kit-tang/',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
