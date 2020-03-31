820a088db8690e4a52a7fdab4e5ce15e12a065dc
CK TANG <cktangad@connect.ust.hk>
2020-03-31 19:30:27 +0800

    Updated footnotes and descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Portfolio of Cheuk Kit Tang'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Click to see works by Cheuk Kit Tang!'} />
       </Helmet>
       <App />
     </>
